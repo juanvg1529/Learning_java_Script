f7bf2ac333a2a986eb05f721bd606a99c2271d81
juanvg1529 <67924398+juanvg1529@users.noreply.github.com>
2021-04-13 12:36:23 -0500

    types of variables of js
    Here I learned about the types of variables used commonly in js such as "le"t and "const"

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hello world");
+console.log("Hello world again");
+let name = "juan"; /*something that can be changed or updated*/
+const ID = 1035873781;/* a variable that will not change*/ 
+
+name= "Diego";
+console.log(name);
+console.log(ID);
+
+/*
+AND &&
+OR ||
+NOT !
+
+*/
+let n1 = 19;
+let n2 = 4;
+let s1 = "hello";
+let s2 = "ciao";
+let b1 = true;
+let b2 = false;
+console.log(n1+n2);
+console.log(s1+s2);/*here i concatenate strings */
+
+console.log(b1!==b2);
